5ec109c7f72ef661c07724fc35fbfe867fe88191
Sonia Kurtz <kurtzsonia@gmail.com>
2017-10-28 12:47:00 +0200

    Added status codes, PointOfInterest Controller, and serialization

--- CityInfo.API/Models/CityDto.cs
+++ CityInfo.API/Models/CityDto.cs
@@ -1,4 +1,6 @@
-﻿namespace CityInfo.API.Models
+﻿using System.Collections.Generic;
+
+namespace CityInfo.API.Models
 {
     public class CityDto
     {
@@ -8,6 +10,15 @@
 
         public string Description { get; set; }
 
-        public int NumberOfPointsOfInterest { get; set; }
+        public int NumberOfPointsOfInterest
+        {
+            get
+            {
+                return PointsOfInterest.Count;
+            }
+        }
+
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; }
+        = new List<PointOfInterestDto>();
     }
 }
--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CityInfo.API
@@ -11,7 +11,9 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .AddMvcOptions(o => o.OutputFormatters.Add(
+                    new XmlDataContractSerializerOutputFormatter()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -21,6 +23,8 @@ namespace CityInfo.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseStatusCodePages();
 
             app.UseMvc();
 
